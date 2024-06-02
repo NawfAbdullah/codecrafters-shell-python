80662d905961f508e97bf05de3352f24571e08b1
Nawf Abdullah <k.nawfabdullah@gmail.com>
2024-06-02 23:30:05 +0530

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -6,14 +6,25 @@ def main():
     # print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
+    commands = {
+        'echo':'',
+        'exit':'',
+        'type':''
+    }
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()   
         command = input()
+        
         if command.split(' ')[0]=='exit':
             sys. exit(0) 
         elif command.split(' ')[0]=='echo':
             sys.stdout.write(' '.join(command.split(' ')[1:])+'\n')
+        elif command.split(' ')[0]=='type':
+            if(command.split(' ')[1] in commands.keys()):
+                print(command.split(' ')[1]+' is a shell builtin')
+            else:
+                print(command.split(' ')[1]+' not found')
         else:
             print(f"{command}: command not found")
 
