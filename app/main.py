ccb767a5cb9de430ec3d14f9790a9b5d3b0a80ab
Nawf Abdullah <k.nawfabdullah@gmail.com>
2024-06-07 10:52:03 +0530

    pass 10th stage

--- app/main.py
+++ app/main.py
@@ -1,17 +1,48 @@
 import sys
 import os
-
+import subprocess
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     # print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
+
+    def echo(_):
+        sys.stdout.write(' '.join(command.split(' ')[1:])+'\n')
+
+
+
+    def handle_type(cmd):
+        cmd = command.split(" ")[1]
+        cmd_path = None
+        paths = PATH.split(":")
+        for path in paths:
+            if os.path.isfile(f"{path}/{cmd}"):
+                cmd_path = f"{path}/{cmd}"
+            
+        if cmd in commands.keys():
+            sys.stdout.write(f"{cmd} is a shell builtin\n")
+        elif cmd_path:
+            sys.stdout.write(f"{cmd} is {cmd_path}\n")
+        else:
+            sys.stdout.write(f"{cmd} not found\n")
+
+
     commands = {
-        'echo':'',
-        'exit':'',
-        'type':''
+        'echo':echo,
+        'exit':lambda  _:sys.exit(0) ,
+        'type':handle_type
     }
+
+
+    def locate_executable(command):
+        path = os.environ.get("PATH", "")
+        for directory in path.split(":"):
+            file_path = os.path.join(directory, command)
+            if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+                return file_path
+
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()   
@@ -19,30 +50,15 @@ def main():
 
         PATH = os.environ.get("PATH")
         
-        if command.split(' ')[0]=='exit':
-            sys. exit(0) 
-        elif command.split(' ')[0]=='echo':
-            sys.stdout.write(' '.join(command.split(' ')[1:])+'\n')
-        elif command.split(' ')[0]=='type':
-
-            cmd = command.split(" ")[1]
-            cmd_path = None
-            paths = PATH.split(":")
-            for path in paths:
-                if os.path.isfile(f"{path}/{cmd}"):
-                    cmd_path = f"{path}/{cmd}"
-            
-            if cmd in commands.keys():
-                sys.stdout.write(f"{cmd} is a shell builtin\n")
-            elif cmd_path:
-                sys.stdout.write(f"{cmd} is {cmd_path}\n")
-            else:
-                sys.stdout.write(f"{cmd} not found\n")
-
+        if command.split(' ')[0] in commands.keys():
+            commands[command.split(' ')[0]](command)
+      
         else:
-            print(command.split(" ")[0])
-            if os.path.isfile(command.split(" ")[0]):
-                os.system(command)
+            main_cmd,*args = command.split(' ')
+            executable = locate_executable(main_cmd)
+            if executable:
+                print("execiting")
+                subprocess.run([executable, *args])
             else:
                 print(f"{command}: command not found")
 
